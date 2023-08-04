9df5804509a9d45190d599f26b6863ee6277ed28
이진수 <leejinsoo0412@gmail.com>
2023-08-04 11:23:35 +0900

    item 안주워지는 현상 수정

--- rpg unity/Assets/character/script/ItemMagnet.cs
+++ rpg unity/Assets/character/script/ItemMagnet.cs
@@ -7,6 +7,8 @@ public class ItemMagnet : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("??");
+        Debug.Log(collision.name);
         if (collision.gameObject.tag == "Item")
         {
             collision.transform.GetChild(1).gameObject.SetActive(true);
@@ -14,6 +16,7 @@ public class ItemMagnet : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
+        Debug.Log("???");
         if (collision.gameObject.tag == "Item")
         {
             collision.transform.GetChild(1).gameObject.SetActive(false);
