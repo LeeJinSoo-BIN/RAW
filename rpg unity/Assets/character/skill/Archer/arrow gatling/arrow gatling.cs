1da45109164b00e9893561f1f165dda814f70c04
이진수 <leejinsoo0412@gmail.com>
2023-08-17 17:58:28 +0900

    skill - arrow gatling 업데이트

--- rpg unity/Assets/character/skill/Archer/arrow gatling/arrow gatling.cs
+++ rpg unity/Assets/character/skill/Archer/arrow gatling/arrow gatling.cs
@@ -7,6 +7,7 @@ public class arrowgatling : MonoBehaviour
 {
     public Vector3 targetPos;
     public float speed = 1;
+    public bool explosion = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +18,21 @@ public class arrowgatling : MonoBehaviour
     void Update()
     {
         Vector3 dir = targetPos - transform.position;
-        if(dir.magnitude < 0.01f)
-            Destroy(gameObject);
+        if (dir.magnitude < 0.01f)
+        {
+            if (explosion)
+            {
+                transform.GetChild(1).gameObject.SetActive(true);
+                transform.GetChild(0).gameObject.SetActive(false);
+                Invoke("destroy_self", 0.3f);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+            
+        }
 
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed);
         //transform.position += dir * speed * Time.deltaTime;
@@ -28,10 +42,14 @@ public class arrowgatling : MonoBehaviour
         float angle_rad = angle_pi * Mathf.Rad2Deg + 180;
         /*if (transform.localScale.x > 0)
             angle_rad -= 180;*/
-        transform.rotation = Quaternion.AngleAxis(angle_rad, Vector3.forward);
-
-
-        
+        transform.rotation = Quaternion.AngleAxis(angle_rad, Vector3.forward);       
         
     }
+
+    void destroy_self()
+    {
+        Destroy(gameObject);
+    }
 }
+
+
